fd477d2c2d345bf076a3a583fef0b5102bbc573c
MIShuvro <manjarulislam75@gmail.com>
2022-10-28 23:57:38 +0600

    init

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+require('dotenv').config({
+  path: process.env.ENV_PATH,
+});
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfigService } from './common/app-config/service/app-config.service';
 
 async function bootstrap() {
+  console.log('PORT======',process.env.PORT);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
 }
